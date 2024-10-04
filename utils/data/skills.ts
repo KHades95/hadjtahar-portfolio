0cbbca39ad28ee3259fb3315e893c3cf3d30708f
devmax214 <campbellstubblebine@gmail.com>
2024-10-04 16:55:52 -0500

    update skills

--- utils/data/skills.ts
+++ utils/data/skills.ts
@@ -4,9 +4,7 @@ export const skillsData = [
 'React',
 'Redux',
 'Next',
-'React-Native',
 'Vue',
-'Angular',
 'Node',
 'Express',
 'Nest',
@@ -14,22 +12,14 @@ export const skillsData = [
 'Django',
 'Laravel',
 'Java',
-'ASP',
-'C#',
 'GraphQL',
 'PostgreSQL',
 'MySQL',
 'MongoDB',
-'Jest',
-'Docker',
 'Git',
-'DigitalOcean',
 'Wordpress',
 'Shopify',
 'NFT',
-'Magento',
-'WordPress',
-'Shopify'
 ]
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
